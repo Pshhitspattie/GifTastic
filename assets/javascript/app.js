982c6b0bca57e3db33a4b65d25921095f3554ac4
Patrycja Gorczynska <patrycjagorczynska@Patrycjas-MacBook-Pro.local>
2018-12-14 21:34:18 -0700

    JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,13 +6,79 @@ var topics = [
     "The Handmaid's Tale",
     "Parks and Recreation",
     "Gossip Girl",
-    "Stranger Things"
-];
+    "Stranger Things" ];
 
-for(var i = 0; i < topics.length; i++) {
-	var button = $("<button>").text(topics[i]);
-	button.attr("data-tv", topics[i]);
-	button.addClass("tv-button");
-	$("#button-group").append(button);
-}
 
+
+    for(var i = 0; i < topics.length; i++) {
+        var button = $("<button>").text(topics[i]);
+        button.attr("data-tv", topics[i]);
+        button.addClass("tv-button");
+        $("#button-group").append(button);
+    }
+    
+    $("#add-tv-button").on("click", function(e) {
+        e.preventDefault();
+        var alreadyExist = false;
+        if(topics.indexOf($("#new-tv-input").val()) !== -1) {
+            alreadyExist = true; 
+        } 
+        if($("#new-tv-input").val() !== "" && alreadyExist === false) {
+            var newTv = $("#new-tv-input").val().toLowerCase();
+            topics.push(newTv);
+            var button = $("<button>").text(newTv);
+            button.attr("data-tv", newTv);
+            button.addClass("tv-button");
+            $("#button-group").append(button);
+        }
+        $("#new-tv-input").val("");
+    });
+    
+    $(document).on("click", ".tv-button", function() {
+        var tv = $(this).attr("data-tv");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            tv + "&api_key=zC95o1xGUhHziFhRt7gjJ56asNrqJa1x&limit=10";
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(response) {
+            var results = response.data;
+            // console.log(results);
+    
+            var resultsContainerSection = $("<section class='results-container'>");
+    
+            for(var i = 0; i < results.length; i++) {
+                var singleResultDiv = $("<div class='result-container'>");
+                
+                var rating = results[i].rating;
+    
+                var p = $("<p>").text("Rating: " + rating);
+    
+                var tvImg = $("<img class='result'>");
+                tvImg.attr("src", results[i].images.fixed_height_still.url);
+                tvImg.attr("data-state", "still");
+                tvImg.attr("data-still", results[i].images.fixed_height_still.url);
+                tvImg.attr("data-animate", results[i].images.fixed_height.url);
+    
+                singleResultDiv.prepend(tvImg);
+                singleResultDiv.prepend(p);
+    
+                resultsContainerSection.prepend(singleResultDiv);
+            }
+    
+            $("#tv-group").prepend(resultsContainerSection);
+        });
+    });
+    
+    $(document).on("click", ".result", function() {
+        var state = $(this).attr("data-state");
+    
+        if(state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
